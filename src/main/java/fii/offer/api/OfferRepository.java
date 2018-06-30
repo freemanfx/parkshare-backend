5453334343e6d7b5967ee4ffc629e94d39f23940
freemanfx <freemanfx@gmail.com>
2018-06-30 20:27:21 +0300

    return offers that have not expired yet

--- src/main/java/fii/offer/api/OfferRepository.java
+++ src/main/java/fii/offer/api/OfferRepository.java
@@ -11,8 +11,7 @@ public interface OfferRepository extends CrudRepository<Offer, Long> {
     List<Offer> findOfferByParking(Parking parking);
 
     @Query("SELECT o from Offer o WHERE o.booking.userId is NULL" +
-            " AND o.validity.start < CURRENT_DATE" +
-            " AND o.validity.end > CURRENT_DATE" +
+            " AND o.validity.end > CURRENT_TIMESTAMP" +
             " ORDER by o.validity DESC"
     )
     List<Offer> findByBounds(double southWestLatitude, double southWestLongitude, double neLatitude, double neLongitude);
